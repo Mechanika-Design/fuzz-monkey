a181a73dd5a01b33aa33f5be407a153e305ac2f7
Warren Galyen <wgalyen@bellhelmets.com>
2021-04-08 14:32:15 -0700

    Updated tests

---
+++ src/__tests__/utils.mjs
@@ -0,0 +1,14 @@
+import test from 'ava';
+import { stub, spy } from "sinon"
+import * as utils from '../utils.mjs';
+// import * as behaviors from '../behaviors/index.mjs';
+
+test('It should be able to invoke a random behavior;', t => {
+  console.log(utils);
+  t.pass();
+  // stub(behaviors, {first: spy()              });
+  // utils.runBehavior({});
+  // t.is(first.callCount, 1);
+  // t.is(second.callCount, 1);
+  // t.is(third.callCount, 1);
+});
