b13d4681add28e76311963deeca1021f63468df5
Warren Galyen <wgalyen@bellhelmets.com>
2021-04-08 14:08:15 -0700

    Handling errors

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ Fuzz Monkey
 ![screenshot](https://via.placeholder.com/800x400?text=Project+Screenshot "screenshot")
 
 It's important to remember that [monkey testing](https://en.wikipedia.org/wiki/Monkey_testing) should be used in conjunction with smarter tests such as [integration tests](https://en.wikipedia.org/wiki/Integration_testing).
+
+## Getting Started
+
+> node --experimental-modules ./bin/index.mjs --headless=false --url https://bellhelmets.com/
--- src/utils.mjs
+++ src/utils.mjs
@@ -1,8 +1,15 @@
+import * as R from 'ramda';
 import * as behaviors from './behaviors/index.mjs';
 
-export function runBehavior(page) {
+export function runBehavior(params) {
   const keys = Object.keys(behaviors);
   const count = keys.length;
   const name = keys[Math.floor(Math.random() * count)];
-  return behaviors[name](page);
+  return behaviors[name](params);
+}
+
+export function mockNatives(page) {
+  page.exposeFunction('alert', R.identity);
+  page.exposeFunction('confirm', R.identity);
+  page.exposeFunction('prompt', R.identity);
 }
