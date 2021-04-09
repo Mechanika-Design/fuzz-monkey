0c94a71b6a55c9d0d2a35fb3d5fb5cd568b3b582
Warren Galyen <wgalyen@bellhelmets.com>
2021-04-09 08:59:07 -0700

    Added initial typer behavior

--- src/behaviors/clicker.mjs
+++ src/behaviors/clicker.mjs
@@ -1,14 +1,14 @@
-export default async function clicker({ page, helpers }) {
-  try {
-    const { height, width } = await page.evaluate(() => ({
-      height: window.innerHeight,
-      width: window.innerWidth
-    }));
+export default async function clicker ({ page, helpers }) {
+    try {
+        const { height, width } = await page.evaluate(() => ({
+            height: window.innerHeight,
+            width: window.innerWidth
+        }));
 
-    const x = Math.round(Math.random() * height);
-    const y = Math.round(Math.random() * width);
-    await page.mouse.click(x, y);
+        const x = Math.round(Math.random() * height);
+        const y = Math.round(Math.random() * width);
+        await page.mouse.click(x, y);
 
-    return void helpers.log('clicker', `Performed a click at ${x}, ${y}`);
-  } catch {}
+        return void helpers.log('clicker', `Clicked at ${x}, ${y}`);
+    } catch {}
 }
--- src/behaviors/index.mjs
+++ src/behaviors/index.mjs
@@ -1,1 +1,2 @@
 export { default as click } from './clicker.mjs';
+export { default as typer } from './typer.mjs';
