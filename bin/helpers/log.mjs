773bacb98f9e656678f0c6d8da1f9ab8bf550684
Warren Galyen <wgalyen@bellhelmets.com>
2021-04-12 09:23:06 -0700

    Fixed count and count related padding

--- bin/helpers/log.mjs
+++ bin/helpers/log.mjs
@@ -3,8 +3,10 @@ import numeral from 'numeral';
 import capitalise from 'capitalize';
 
 export default function log(current, total) {
-  const getTypeColour = () =>
+  const getTypeColor = () =>
     chalk.level > 1 ? chalk.hex('#ffd2e8') : chalk.magentaBright;
+
+  const total_ = numeral(total).format('0,0');
 
   return (type, ...message) => {
     console.log(
@@ -12,11 +14,11 @@ export default function log(current, total) {
       chalk.whiteBright(
         numeral(current)
           .format('0,0')
-          .padEnd(String(total).length)
+          .padEnd(String(total_).length)
       ),
-      chalk.gray(`(of ${numeral(total).format('0,0')})`),
+      chalk.gray(`(of ${total_})`),
       chalk.gray('•'),
-      getTypeColour().bold(capitalise(type).padEnd(10)),
+      getTypeColor().bold(capitalise(type).padEnd(10)),
       chalk.gray('•'),
       ...message
     );
